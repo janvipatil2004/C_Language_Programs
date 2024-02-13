d349db53e49f3d6a3f9ecd33b31b1c8edfd1ec09
janvipatil2004 <05janvi2004@gmail.com>
2024-02-13 13:27:27 +0530

    [ADDED]:Janvi - C Program for if_else

---
+++ If_Else/nestest.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	
+	printf("\nEnter The a:");
+	scanf("%d",&a);
+	
+	printf("\nEnter The b:");
+	scanf("%d",&b);
+	
+	if(a>b)
+	{
+		printf("\nA is big:");
+	}
+	else
+	{
+		printf("\nBis big:");
+	}
+	
+	return 0;
+}
